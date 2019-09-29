2a44e1c02deb8dbea6af9d7a2d303cb6c491e8a3
Tim <cpldcpu@gmail.com>
2019-09-29 19:19:27 +0200

    add explanation for oscillator trimming

--- Toolchain/examples/blinky/blinky.c
+++ Toolchain/examples/blinky/blinky.c
@@ -15,7 +15,20 @@
 
 unsigned char _sdcc_external_startup(void)
 {
-	CLKMD = CLKMD_IHRC_DIV2|CLKMD_ENABLE_IHRC;  // 8 Mhz main clock
+	CLKMD = CLKMD_IHRC_DIV2|CLKMD_ENABLE_IHRC;  // 16/2=8 Mhz main clock
+
+//  Uncomment this line to use the automatic IHRC trimming function included in the EASYPDKPROG software.
+//	Trimming will only be activated when the program is started for the first time and the resulting
+//  values will be written to flash. This call is required on MCUS without factory calibration, for example the PMS150C.
+//  In addition, it is possible to trim the IHRC to value different from 16MHz. Also when the operating voltage deviates
+//  significantly from 5V it may be sensible to use additional trimming.
+
+//	EASY_PDK_CALIBRATE_IHRC(F_CPU, 5000);		 // tune SYSCLK to 8.0MHz @ 5.00V
+
+//  Uncomment this line to use the preprogrammed trimming values for IHR and Bandgap provided in 
+//  the flash of PFS154/PFS174. This does not require additional trimming after programming and improves turnaround times.
+//  The accuracy should be sufficient if you want to use IHRC=16 MHz default at 5.00V
+
 	PDK_USE_FACTORY_TRIMMING();
 
 	return 0; // perform normal initialization
