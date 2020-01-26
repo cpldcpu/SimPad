30e7cf90a662e2520140e9126ded5ee5fe5a0a6d
Tim <cpldcpu@gmail.com>
2020-01-26 11:52:50 +0100

    chained display - update trimming, decoding

--- Toolchain/examples/chainable_display/display.c
+++ Toolchain/examples/chainable_display/display.c
@@ -17,10 +17,15 @@
 
 #include "delay.c"	// include delay implementation. Needs to be cleaned up later.
 
+uint8_t state;  // S0=forward  S1= Receive
+
+#define	S_forward	0
+#define S_receive 	1
+#define	Endmarker	0x00	// CR  -> Code should not be 0xff to avoid glitches causing updates
+
 uint8_t datareg;
 uint8_t forwardreg;
 uint8_t loopctr;
-uint8_t state=1;  // S0=forward  S1= Receive
 
 #define	Din	PA0
 #define Dout PA6
@@ -38,13 +43,15 @@ uint8_t state=1;  // S0=forward  S1= Receive
 
 #endif
 
-//                     0   1    2    3    4    5    6	 7	  8    9	A	 B 	  C	   D    E	 F
-const char numbers[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x58,0x5e,0x79,0x71};
+// The table is organized in a way, where it will decode both ascii directly and hex digits with an offset of 0x30.
+//					  SPC/@ A	 B 	  C	   D    E	 F    G    H    I    J    K    L    M    N    O     P/0  1    2    3    4    5    6	   7	8    9    A	   B 	C	 D    E	   F
+const char segdec[] ={0x00,0x77,0x7c,0x58,0x5e,0x79,0x71,0x00,0x76,0x30,0x0e,0x00,0x38,0x00,0x54,0x5c, 0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x58,0x5e,0x79,0x71};
 
 unsigned char _sdcc_external_startup(void)
 {
 	CLKMD = CLKMD_IHRC_DIV2 | CLKMD_ENABLE_IHRC; // 8 Mhz main clock
-	PDK_USE_FACTORY_TRIMMING();
+	// PDK_USE_FACTORY_TRIMMING();			 	// It appears that the factory trimming is sometimes slightly off - possibly not done at 5V
+	EASY_PDK_CALIBRATE_IHRC(F_CPU,5000);
 #ifdef debug 
 	PDK_autobaud();
 #endif
@@ -63,15 +70,27 @@ void main(void)
 	__asm__("engint"); // enable interrupts
 
 	PBC=0x7F;		
+	PB=0x7f^0x40;  // Display hyphen '-' after reset
+
 	PAC|=_BV(Dout);
 	forwardreg=255;
-
+	state = S_receive;
 	for (;;)
 	{
-		if ((forwardreg==0)&&(state==0)) {	// reset condition met
-			PB=0x7f^numbers[datareg&15];
-			state=1;
+		if ((forwardreg==Endmarker)&&(state==S_forward)) {	// received end marker while in forwarding mode -> update condition met
+			uint8_t datain=datareg;
+			switch (datain) {
+				// case 0x20: PB=0x7f^0x00; break; // space
+				case '-':  PB=0x7f^0x40; break; // hyphen
+				default:   PB=0x7f^segdec[datain&31]; break;
+			}
+
+			state=S_receive;
 			forwardreg=255;
+			#ifdef debug 
+				PDK_sendchar('U');
+				PDK_sendchar(datain);
+			#endif			
 		}
 	}
 }
@@ -82,15 +101,17 @@ void handler(void) __interrupt(0)
 	// One UART frame last 138.89 cycles at 57600 BAUD and 8 Mhz clock
 	// Entering interrupt handler takes approx 3+2+2=7 cycles
 
-	// Delay to start condition half bit
-	// 139/2-7 ~ 62 cycles
 __asm 
-	t0sn	_state,#0
+	mov		a,#(S_receive)
+	cneqsn	a,_state
 	goto	h_receive
+
+	// Delay to start condition half bit
+	// 139/2-7-3 ~ 59.5 cycles
 
 	// forward
 h_forward:
-	mov		a,#21	// 20*3+2=62 cycles
+	mov		a,#20	// 19*3+2=59 cycles
 h_0$:				
 	dzsn	a		// 3 cycles per loop, 2 for last
 	goto	h_0$
@@ -131,7 +152,10 @@ h_2$:
 	goto	h_exit
 h_receive:
 
-	mov		a,#21	// 20*3+2=62 cycles
+	// Delay to start condition half bit
+	// 139/2-7-4 ~ 58.5 cycles
+
+	mov		a,#20	// 19*3+2=59 cycles
 h_3$:				
 	dzsn	a		// 3 cycles per loop, 2 for last
 	goto	h_3$
@@ -151,7 +175,7 @@ h_4$:
 	mov		_loopctr,a
 
 // receive loop
-// overhead is 5+3=8 cycles
+// overhead is 2+3=5 cycles
 
 h_bitloop2:
 	swapc	s(PA), #(Din)  // sample input 
@@ -167,6 +191,8 @@ h_5$:
 
 	set1 	s(PA), #(Dout)	// complete last bit, stop bit is omitted.
 
+	mov 	a,#(Endmarker)
+	ceqsn 	a,_datareg		// Check if end-marker was received instead of data. If yes, stay in receive mode.
 	clear 	_state			// switch to forward
 h_glitch:
 h_exit:
--- Toolchain/library/pdk/io_common.h
+++ Toolchain/library/pdk/io_common.h
@@ -9,8 +9,8 @@
 /* Generate code with for calibration. Make sure your programmer supports it.
 */
 
-//#define EASY_PDK_CALIBRATE_IHRC(frequency,millivolt) EASY_PDK_CALIBRATE_IHRC_H8(frequency,millivolt)
-#define EASY_PDK_CALIBRATE_IHRC(frequency,millivolt) EASY_PDK_CALIBRATE_IHRC_H9(frequency,millivolt)
+#define EASY_PDK_CALIBRATE_IHRC(frequency,millivolt) EASY_PDK_CALIBRATE_IHRC_H8(frequency,millivolt)
+// #define EASY_PDK_CALIBRATE_IHRC(frequency,millivolt) EASY_PDK_CALIBRATE_IHRC_H9(frequency,millivolt)
 
 #define EASY_PDK_CALIBRATE_IHRC_H8(frequency,millivolt) \
 __asm__(                      \
@@ -90,106 +90,106 @@ __asm__(                      \
 #define INTEN_TM3                    0x80
 
 //interrupt request definitions
- #define INTRQ_PA0                    0x01
- #define INTRQ_PB5                    0x01
- #define INTRQ_PB0                    0x02
- #define INTRQ_PA4                    0x02
- #define INTRQ_T16                    0x04
- #define INTRQ_ADC                    0x08
- #define INTRQ_COMP                   0x10
- #define INTRQ_PWMG                   0x20
- #define INTRQ_TM2                    0x40
- #define INTRQ_TM3                    0x80
+#define INTRQ_PA0                    0x01
+#define INTRQ_PB5                    0x01
+#define INTRQ_PB0                    0x02
+#define INTRQ_PA4                    0x02
+#define INTRQ_T16                    0x04
+#define INTRQ_ADC                    0x08
+#define INTRQ_COMP                   0x10
+#define INTRQ_PWMG                   0x20
+#define INTRQ_TM2                    0x40
+#define INTRQ_TM3                    0x80
 
 //tm16 definitions
- #define T16_INTSRC_8BIT              0x00
- #define T16_INTSRC_9BIT              0x01
- #define T16_INTSRC_10BIT             0x02
- #define T16_INTSRC_11BIT             0x03
- #define T16_INTSRC_12BIT             0x04
- #define T16_INTSRC_13BIT             0x05
- #define T16_INTSRC_14BIT             0x06
- #define T16_INTSRC_15BIT             0x07
- #define T16_CLK_DIV1                 0x00
- #define T16_CLK_DIV4                 0x08
- #define T16_CLK_DIV16                0x10
- #define T16_CLK_DIV64                0x18
- #define T16_CLK_DISABLE              0x00
- #define T16_CLK_SYSCLK               0x20
- #define T16_CLK_PA4_FALL             0x60
- #define T16_CLK_IHRC                 0x80
- #define T16_CLK_ILRC                 0xC0
- #define T16_CLK_PA0_FALL             0xE0
+#define T16_INTSRC_8BIT              0x00
+#define T16_INTSRC_9BIT              0x01
+#define T16_INTSRC_10BIT             0x02
+#define T16_INTSRC_11BIT             0x03
+#define T16_INTSRC_12BIT             0x04
+#define T16_INTSRC_13BIT             0x05
+#define T16_INTSRC_14BIT             0x06
+#define T16_INTSRC_15BIT             0x07
+#define T16_CLK_DIV1                 0x00
+#define T16_CLK_DIV4                 0x08
+#define T16_CLK_DIV16                0x10
+#define T16_CLK_DIV64                0x18
+#define T16_CLK_DISABLE              0x00
+#define T16_CLK_SYSCLK               0x20
+#define T16_CLK_PA4_FALL             0x60
+#define T16_CLK_IHRC                 0x80
+#define T16_CLK_ILRC                 0xC0
+#define T16_CLK_PA0_FALL             0xE0
 
 //eosc definitions
- #define EOSC_LVD_BANDGAP_SHUTDOWN    0x01
+#define EOSC_LVD_BANDGAP_SHUTDOWN    0x01
 
 //integs definitions
- #define INTEGS_PA0_BOTH              0x00
- #define INTEGS_PA0_RISING            0x01
- #define INTEGS_PA0_FALLING           0x02
- #define INTEGS_T16_RISING            0x00
- #define INTEGS_T16_FALLING           0x04
- #define INTEGS_COMP_BOTH             0x00
- #define INTEGS_COMP_RISING           0x40
- #define INTEGS_COMP_FALLING          0x80
+#define INTEGS_PA0_BOTH              0x00
+#define INTEGS_PA0_RISING            0x01
+#define INTEGS_PA0_FALLING           0x02
+#define INTEGS_T16_RISING            0x00
+#define INTEGS_T16_FALLING           0x04
+#define INTEGS_COMP_BOTH             0x00
+#define INTEGS_COMP_RISING           0x40
+#define INTEGS_COMP_FALLING          0x80
 
 //padie definitions
- #define PADIE_PA0_WAKEUP_ENABLE      0x01
- #define PADIE_PA3_WAKEUP_ENABLE      0x08
- #define PADIE_PA4_WAKEUP_ENABLE      0x10
- #define PADIE_PA5_WAKEUP_ENABLE      0x20
- #define PADIE_PA6_WAKEUP_ENABLE      0x40
- #define PADIE_PA7_WAKEUP_ENABLE      0x80
+#define PADIE_PA0_WAKEUP_ENABLE      0x01
+#define PADIE_PA3_WAKEUP_ENABLE      0x08
+#define PADIE_PA4_WAKEUP_ENABLE      0x10
+#define PADIE_PA5_WAKEUP_ENABLE      0x20
+#define PADIE_PA6_WAKEUP_ENABLE      0x40
+#define PADIE_PA7_WAKEUP_ENABLE      0x80
 
 //misc definitions
- #define MISC_WATCHDOG_8K_ILRC        0x00
- #define MISC_WATCHDOG_16K_ILRC       0x01
- #define MISC_WATCHDOG_64K_ILRC       0x02
- #define MISC_WATCHDOG_256K_ILRC      0x03
- #define MISC_LVR_DISABLE             0x04
- #define MISC_FAST_WAKEUP_ENABLE      0x20
+#define MISC_WATCHDOG_8K_ILRC        0x00
+#define MISC_WATCHDOG_16K_ILRC       0x01
+#define MISC_WATCHDOG_64K_ILRC       0x02
+#define MISC_WATCHDOG_256K_ILRC      0x03
+#define MISC_LVR_DISABLE             0x04
+#define MISC_FAST_WAKEUP_ENABLE      0x20
 
 //tm2c definitions
- #define TM2C_CLK_DISABLE             0x00
- #define TM2C_CLK_SYSCLK              0x10
- #define TM2C_CLK_IHRC                0x20
- #define TM2C_CLK_EOSC                0x30
- #define TM2C_CLK_ILRC                0x40
- #define TM2C_CLK_COMPOUT             0x50
- #define TM2C_CLK_PA0_RISE            0x80
- #define TM2C_CLK_PA0_FALL            0x90
- #define TM2C_CLK_PB0_RISE            0xA0
- #define TM2C_CLK_PB0_FALL            0xB0
- #define TM2C_CLK_PA4_RISE            0xC0
- #define TM2C_CLK_PA4_FALL            0xD0
- #define TM2C_OUT_DISABLE             0x00
- #define TM2C_OUT_PB2                 0x04
- #define TM2C_OUT_PA3                 0x08
- #define TM2C_OUT_PB4                 0x0C
- #define TM2C_MODE_PERIOD             0x00
- #define TM2C_MODE_PWM                0x02
- #define TM2C_INVERT_OUT              0x01
+#define TM2C_CLK_DISABLE             0x00
+#define TM2C_CLK_SYSCLK              0x10
+#define TM2C_CLK_IHRC                0x20
+#define TM2C_CLK_EOSC                0x30
+#define TM2C_CLK_ILRC                0x40
+#define TM2C_CLK_COMPOUT             0x50
+#define TM2C_CLK_PA0_RISE            0x80
+#define TM2C_CLK_PA0_FALL            0x90
+#define TM2C_CLK_PB0_RISE            0xA0
+#define TM2C_CLK_PB0_FALL            0xB0
+#define TM2C_CLK_PA4_RISE            0xC0
+#define TM2C_CLK_PA4_FALL            0xD0
+#define TM2C_OUT_DISABLE             0x00
+#define TM2C_OUT_PB2                 0x04
+#define TM2C_OUT_PA3                 0x08
+#define TM2C_OUT_PB4                 0x0C
+#define TM2C_MODE_PERIOD             0x00
+#define TM2C_MODE_PWM                0x02
+#define TM2C_INVERT_OUT              0x01
 
 //tm2s definitions
- #define TM2S_PWM_RES_8BIT            0x00
- #define TM2S_PWM_RES_6BIT            0x80
- #define TM2S_PRESCALE_NONE           0x00
- #define TM2S_PRESCALE_DIV4           0x20
- #define TM2S_PRESCALE_DIV16          0x40
- #define TM2S_PRESCALE_DIV64          0x60
+#define TM2S_PWM_RES_8BIT            0x00
+#define TM2S_PWM_RES_6BIT            0x80
+#define TM2S_PRESCALE_NONE           0x00
+#define TM2S_PRESCALE_DIV4           0x20
+#define TM2S_PRESCALE_DIV16          0x40
+#define TM2S_PRESCALE_DIV64          0x60
 
 //gpcc definitions
- #define GPCC_COMP_PLUS_VINT_R        0x00
- #define GPCC_COMP_PLUS_PA4           0x01
- #define GPCC_COMP_MINUS_PA3          0x00
- #define GPCC_COMP_MINUS_PA4          0x02
- #define GPCC_COMP_MINUS_BANDGAP      0x04
- #define GPCC_COMP_MINUS_VINT_R       0x06
- #define GPCC_COMP_ENABLE             0x80
- #define GPCC_COMP_RESULT             0x40
- #define GPCC_COMP_SYNC_TM2           0x20
- #define GPCC_COMP_INVERSE            0x10
+#define GPCC_COMP_PLUS_VINT_R        0x00
+#define GPCC_COMP_PLUS_PA4           0x01
+#define GPCC_COMP_MINUS_PA3          0x00
+#define GPCC_COMP_MINUS_PA4          0x02
+#define GPCC_COMP_MINUS_BANDGAP      0x04
+#define GPCC_COMP_MINUS_VINT_R       0x06
+#define GPCC_COMP_ENABLE             0x80
+#define GPCC_COMP_RESULT             0x40
+#define GPCC_COMP_SYNC_TM2           0x20
+#define GPCC_COMP_INVERSE            0x10
 
 #if defined __SDCC_pdk13
  #define GPCC_COMP_MINUS_PA6          0x08
@@ -200,12 +200,12 @@ __asm__(                      \
 #endif
 
 //gpcs definitions
- #define GPCS_COMP_CASE1              0x00
- #define GPCS_COMP_CASE2              0x10
- #define GPCS_COMP_CASE3              0x20
- #define GPCS_COMP_CASE4              0x30
- #define GPCS_COMP_WAKEUP_ENABLE      0x40
- #define GPCS_COMP_OUTPUT_PA0         0x80
+#define GPCS_COMP_CASE1              0x00
+#define GPCS_COMP_CASE2              0x10
+#define GPCS_COMP_CASE3              0x20
+#define GPCS_COMP_CASE4              0x30
+#define GPCS_COMP_WAKEUP_ENABLE      0x40
+#define GPCS_COMP_OUTPUT_PA0         0x80
 
 //adcc definitions
 #define ADCC_ADC_ENABLE              0x80
